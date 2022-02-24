8ec2ffcca7be1717fc752662f949948731fd954b
stravs <stravs.a@gmail.com>
2022-02-24 18:52:59 +0100

    changed footer to 2023

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ export default class Footer extends React.Component {
        <div className="container text-center">
   
 
-         <p className="text-muted small text-center mb-5">Heliski Albania 2020</p>
+         <p className="text-muted small text-center mb-5">Heliski Albania 2022</p>
        </div>
       </footer>
     );
