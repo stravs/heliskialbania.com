1b21ba2a3f890baf9b7c227d1ad93f32785e9b6f
stravs <stravs.a@gmail.com>
2024-02-23 18:57:02 +0100

    Update packages.js changed flying time to rotor time

--- src/pages/packages.js
+++ src/pages/packages.js
@@ -16,8 +16,8 @@ const PackagesPage = ({data}) => (
 	    }}
 	    >
 	    <SectionRight altText="People having fun on pick up zone" sectionImage={data.hobsImage} 
-      title="What do I get for packages based on flying time?" 
-      text="We strongly believe this is the most honest way for our clients when buying heliskiing packages. With the proximity of 6-10 minute flying time to our prime terrain from our base in Valbona there will be minimum time spent for refuel and ferry flights from one zone to another. Four hours of flying time, with the terrain that we fly, is roughly translated into 25-30 runs per group with runs from 400-1600 vertical meters. 
+      title="What do I get for packages based on rotor time?" 
+      text="We strongly believe this is the most honest way for our clients when buying heliskiing packages. With the proximity of 6-10 minute rotor time to our prime terrain from our base in Valbona there will be minimum time spent for refuel and ferry flights from one zone to another. Four hours of rotor time, with the terrain that we fly, is roughly translated into 25-30 runs per group with runs from 400-1600 vertical meters. 
 "/>
 {/*      <div className="container text-center py-2 py-lg-5">
         <div className="row">
@@ -45,11 +45,11 @@ const PackagesPage = ({data}) => (
 				altText="Nice powder turn"
 				sectionImage={data.weekPackageImage}
 				title="One week heliskiing"
-				description="This is a classic heliskiing package based on 4 hours of fly time. Why don’t we sell vertical meters? When buying flying time we guarantee you to have the best possible experience and terrain. We will fly to the areas with the best snow and conditions and also to the terrain that suits you group best.  You will share your seat in a group of 4 skiers and  a guide. We warmly encourage you to book your seats together in a group of 4 buddies that are at the same level of skiing and therefore have the best possible experience. There will be maximum 3 groups per helicopter on the snow at the same time. Normally there is no waiting time for any of the groups with this set up. Are you more than 4 in your group ? Than consider booking Private Heliski Albania package for best possible experience and have the entire operation for yourself!"
-				heli="1 helicopter - small group of 4 skiers 1 guide - 3 groups max per 1 helicopter - 4 hours of flying time per 1 group of 4 skiers"
-				includes="The package includes 6 nights with full board accommodation, 4 hours of flying time, 5 possible days of skiing, IFMGA mountain guides"
+				description="This is a classic heliskiing package based on 4 hours of fly time. Why don’t we sell vertical meters? When buying rotor time we guarantee you to have the best possible experience and terrain. We will fly to the areas with the best snow and conditions and also to the terrain that suits you group best.  You will share your seat in a group of 4 skiers and  a guide. We warmly encourage you to book your seats together in a group of 4 buddies that are at the same level of skiing and therefore have the best possible experience. There will be maximum 3 groups per helicopter on the snow at the same time. Normally there is no waiting time for any of the groups with this set up. Are you more than 4 in your group ? Than consider booking Private Heliski Albania package for best possible experience and have the entire operation for yourself!"
+				heli="1 helicopter - small group of 4 skiers 1 guide - 3 groups max per 1 helicopter - 4 hours of rotor time per 1 group of 4 skiers"
+				includes="The package includes 6 nights with full board accommodation, 4 hours of rotor time, 5 possible days of skiing, IFMGA mountain guides"
 				price="8 950€ per person"
-				priceSecond="Extra hour of flying time - 3 500€ per group"
+				priceSecond="Extra hour of rotor time - 3 500€ per group"
 			/>
 
 			<Section altText="Nice turn with a scenic background" sectionImage=
@@ -65,12 +65,12 @@ const PackagesPage = ({data}) => (
       stay." 
       heli="1 helicopter only for your group - entire
       operation only for your group - up to 8 people in a group - 8
-      hours of flying time" 
+      hours of rotor time" 
       includes="The package includes 6
-      nights with full board accommodation, 8 hours of flying time,
+      nights with full board accommodation, 8 hours of rotor time,
       5 possible days of skiing, IFMGA mountain guides" 
       price="Price for 8 people, 8 hours 95 000€ per group"
-      priceThird="Extra (hour) of flying time - 3 500€ per group" 
+      priceThird="Extra (hour) of rotor time - 3 500€ per group" 
       />
     </div>
   </div>
