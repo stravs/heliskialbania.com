70a7e917e0d9b74338681101a916916f3da9f8cd
stravs <stravs.a@gmail.com>
2024-09-06 18:33:06 +0200

    Update section-safety.js

--- src/components/section-safety.js
+++ src/components/section-safety.js
@@ -25,7 +25,6 @@ export default class SectionSafety extends React.Component {
             <div className="offset-lg-1 col-lg-3">
               <h1 className="card-title">Safety is our priority</h1>
               <p className="card-text">This is a small operation  running under strict protocols and safety standards. We are aware that any activity in the mountains involves risks, especially when heliskiing, covering lots of ground in short time. Therefore we will do our best and make safety our priority. <br></br> Combining knowledge from our team, with constant education we are creating the best possible experience for our clients.</p>
-              <a href="/safety" className="btn btn-primary">See how we operate</a>
             </div>
           </div>
         </div>
